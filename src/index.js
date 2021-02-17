8b7e433d1adaebd1b30f9c50fa4ae097e442ff0d
Chamy <yd95069506@naver.com>
2021-02-17 23:05:36 +0900

    JSX

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+      return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>
+}
+
+const user = {
+  firstName: 'Marco',
+  lastName: 'Polo'
+};
+
+const element = getGreeting(user);
+
 ReactDOM.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>,
-  <h1>Hello World</h1>,
+  element,
   document.getElementById('root')
 );
 
