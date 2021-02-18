d4f2d385c3aa13ecf1788701f285462f1b51231a
Chamy <yd95069506@naver.com>
2021-02-18 22:33:35 +0900

    엘리먼트 렌더링

--- src/index.js
+++ src/index.js
@@ -3,28 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
+
+  ReactDOM.render(element, document.getElementById('root'));
 }
 
-function getGreeting(user) {
-  if (user) {
-      return <h1>Hello, {formatName(user)}!</h1>;
-  }
-  return <h1>Hello, Stranger.</h1>
-}
-
-const user = {
-  firstName: 'Marco',
-  lastName: 'Polo'
-};
-
-const element = getGreeting(user);
-
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
