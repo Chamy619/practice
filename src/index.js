bf6e0e0d4a8ca466137ea3ecd26ff6cb20612146
Chamy <yd95069506@naver.com>
2021-02-19 22:02:17 +0900

    Component and props

--- src/index.js
+++ src/index.js
@@ -1,20 +1,46 @@
+import { checkPropTypes } from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
-
-  ReactDOM.render(element, document.getElementById('root'));
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-setInterval(tick, 1000);
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
