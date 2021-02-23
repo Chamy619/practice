823ea157aad6420b3f7171c3c97b164e13f4f63c
Chamy <yd95069506@naver.com>
2021-02-23 22:21:39 +0900

    리스트와 Key

--- src/index.js
+++ src/index.js
@@ -4,45 +4,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function WarningBanner(props) {
-  if (!props.warn) {
-    return null;
-  }
-
+function ListItem(props) {
+  const value = props.value;
   return (
-    <div className="warning">
-      Warning!
-    </div>
+    <li>
+      {value}
+    </li>
   );
 }
 
-class Page extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {showWarning: true};
-    this.handleToggleClick = this.handleToggleClick.bind(this);
-  }
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map(number => <ListItem value = {number} key = {number}/>);
 
-  handleToggleClick() {
-    this.setState(state => ({
-      showWarning: !state.showWarning
-    }));
-  }
-
-  render() {
-    return (
-      <div>
-        <WarningBanner warn = {this.state.showWarning} />
-        <button onClick = {this.handleToggleClick}>
-          {this.state.showWarning ? 'Hide' : 'Show'}
-        </button>
-      </div>
-    );
-  }
+  return (
+    <ul>
+      {listItems}
+    </ul>
+  );
 }
 
-ReactDOM.render(<Page />, document.getElementById('root'));
+const numbers = [1, 2, 3, 4, 5];
 
+ReactDOM.render(<NumberList numbers = {numbers} />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
