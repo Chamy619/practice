2d519435851d634da35f9eef2c9b811aaee809dc
Chamy <yd95069506@naver.com>
2021-02-24 23:20:26 +0900

    폼

--- src/index.js
+++ src/index.js
@@ -4,29 +4,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function ListItem(props) {
-  const value = props.value;
-  return (
-    <li>
-      {value}
-    </li>
-  );
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Is going: ' + this.state.isGoing + '\n number of guests: ' + this.state.numberOfGuests);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Is going: 
+          <input type="checkbox" name="isGoing" checked={this.state.isGoing} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input type="number" name="numberOfGuests" value={this.state.numberOfGuests} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
 }
 
-function NumberList(props) {
-  const numbers = props.numbers;
-  const listItems = numbers.map(number => <ListItem value = {number} key = {number}/>);
-
-  return (
-    <ul>
-      {listItems}
-    </ul>
-  );
-}
-
-const numbers = [1, 2, 3, 4, 5];
-
-ReactDOM.render(<NumberList numbers = {numbers} />, document.getElementById('root'));
+ReactDOM.render(<Reservation />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
